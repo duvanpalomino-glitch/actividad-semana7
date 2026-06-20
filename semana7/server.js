18ca2234e5d81edbcae7d8ac8b3d5a33c1c2facf
duvanpalomino-glitch <juandiegojuliopalomino@DiegoRemix.local>
2026-06-19 22:36:05 -0500

    actividad-semana7

---
+++ semana7/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+
+const port = 3000
+
+app.use(express.json());
+
+let reportes = [];
+
+app.get('/reportes', (req, res) => {
+  res.json(reportes);
+});
+
+app.post('/reportes', (req, res) => {
+
+  const reporte = {
+    id: reportes.length + 1,
+    tipo: req.body.tipo,
+    descripcion: req.body.descripcion
+  };
+
+  reportes.push(reporte);
+
+  res.json({
+    mensaje: "Reporte registrado",
+    reporte: reporte
+  });
+
+});
+
+app.listen(port, () => {
+  console.log(`Servidor escuchando en http://localhost:${port}`);
+})
